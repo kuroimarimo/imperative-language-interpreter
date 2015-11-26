2a4e5f2339041323f70bc29b81252b5e1e296160
Valentine <valentina.strakova@gmail.com>
2015-11-26 02:34:08 +0100

    Edited changed enum names from lex_an.h; please change gramatic's ifs to a switch.

--- prsa.c
+++ prsa.c
@@ -11,28 +11,38 @@ error propojka
 
 return prec.SA???
 */
+
+#include "error.h"
 #include "prsa.h"
 
 //--------------------------//
 //		ZASOBNIK			//
 //--------------------------//
-void Init (tList *Z) {											// inicializace
+
+void Init (tList *Z)
+{                                                               // inicializace
 	Z->First = NULL;
 }
 
-void Dispose (tList *Z) {
-	tElemPtr ukazatel = NULL;									// deklarace pro predani ukazatele	
-	while(Z->First != NULL) {									// cyklus pro free() zasobniku
-		ukazatel = Z->First->ptr;
+void Dispose (tList *Z)
+{
+	tElemPtr pointer = NULL;									// deklarace pro predani ukazatele
+	
+    while(Z->First != NULL)
+    {                                                           // cyklus pro free() zasobniku
+		pointer = Z->First->ptr;
 		free(Z->First);
-		Z->First = ukazatel;
+		Z->First = pointer;
 	}
 	Init(Z);													// reinicializace zasobniku
 }
 
-void FirstTerminal (tList *Z, int *val) {
-	if (Z->First != NULL) {
+void FirstTerminal (tList *Z, int *val)
+{
+	if (Z->First != NULL)
+    {
 		*val = Z->First->data;
+        
 		while (val == 'E') 										// hledam terminal
 			if (Z->First->ptr != NULL) 
 				*val = Z->First->data;							// aktualni terminal
@@ -41,25 +51,36 @@ void FirstTerminal (tList *Z, int *val) {
 	//else error
 }
 
-void Push (tList *Z, int val) {
-	tElemPtr ukazatel = NULL;
-	if(Z->First != NULL) ukazatel = Z->First;
+void Push (tList *Z, int val)
+{
+	tElemPtr pointer = NULL;
+    
+	if(Z->First != NULL)
+        pointer = Z->First;
+    
 	tElemPtr struktura = malloc(sizeof(struct tElem));			// alokace struktury
-	if(struktura != NULL) {										// kontrola alokace
+	
+    if(struktura != NULL)
+    {                                                           // kontrola alokace
 	 	Z->First = struktura;									// prirazeni a naplneni
 	 	struktura->data = val;
-	 	struktura->ptr = ukazatel;
+	 	struktura->ptr = pointer;
 	}
 	//else error
 }
 
-void Pop (tList *Z, int *val) {
-	if (Z->First != NULL) {
+void Pop (tList *Z, int *val)
+{
+	if (Z->First != NULL)
+    {
 		*val = Z->First->data;									// pop hodnoty
-		tElemPtr ukazatel = NULL; 								// deklarace pro presun ukazatelu
-		if (Z->First->ptr != NULL) ukazatel = Z->First->ptr; 	// uchovani ukazatele
-		free(Z->First);
-		Z->First = ukazatel;									// posunuti fronty
+		tElemPtr pointer = NULL; 								// deklarace pro presun ukazatelu
+        
+		if (Z->First->ptr != NULL)
+            pointer = Z->First->ptr;                            // uchovani ukazatele
+		
+        free(Z->First);
+		Z->First = pointer;                                     // posunuti fronty
 	}
 	//else error
 }
@@ -67,55 +88,72 @@ void Pop (tList *Z, int *val) {
 //--------------------------//
 //		GRAMATIKA			//
 //--------------------------//
-int PrecG(int suc1, int suc2, int suc3) {
-switch (suc1) {
-	case 'E':
-		if (suc3 == 'E') {
-			if (suc2 == PLUS) {					// pro E -> E+E
-				printf("E -> E+E\n");
-				return 1; 							// return 1
-			}
-			if (suc2 == MINUS) {				// pro E -> E-E
-				printf("E -> E-E\n");
-				return 2;							// return 2 
-			}
-			if (suc2 == MULTIPLY) {				// pro E -> E*E
-				printf("E -> E*E\n");
-				return 3; 							// return 3
-			}
-			if (suc2 == DIVIDE) {				// pro E -> E/E
-				printf("E -> E/E\n");
-				return 4; 							// return 4
-			}
-			if (suc2 == GREATER) {				// pro E -> E>E
-				printf("E -> E>E\n");
-				return 5;							// return 5
-			}
-			if (suc2 == GREATER_ROVNO) {		// pro E -> E>=E
-				printf("E -> E>=E\n");
-				return 6;							// return 6
-			}
-			if (suc2 == LESS) {					// pro E -> E<E
-				printf("E -> E<E\n");
-				return 7;							// return 7
-			}
-			if (suc2 == LESS_ROVNO) {			// pro E -> E<=E
-				printf("E -> E<=E\n");
-				return 8;							// return 8
-			}
-			if (suc2 == EQUAL) {				// pro E -> E==E
-				printf("E -> E==E\n");
-				return 9;							// return 9
-			}
-			if (suc2 == NEGACE) {				// pro E -> E!=E
-				printf("E -> E!=E\n");
-				return 10;							// return 10
-			}
-		}
+
+int PrecG(int suc1, int suc2, int suc3)
+{
+    switch (suc1)
+    {
+	
+        case 'E':
+            if (suc3 == 'E')
+            {
+                if (suc2 == PLUS)
+                {                               // pro E -> E+E
+                    printf("E -> E+E\n");
+                    return 1; 							// return 1
+                }
+                if (suc2 == MINUS)
+                {                               // pro E -> E-E
+                    printf("E -> E-E\n");
+                    return 2;							// return 2
+                }
+                if (suc2 == MULTIPLY)
+                {                               // pro E -> E*E
+                    printf("E -> E*E\n");
+                    return 3; 							// return 3
+                }
+                if (suc2 == DIVIDE)
+                {                               // pro E -> E/E
+                    printf("E -> E/E\n");
+                    return 4; 							// return 4
+                }
+                if (suc2 == GREATER)
+                {                               // pro E -> E>E
+                    printf("E -> E>E\n");
+                    return 5;							// return 5
+                }
+                if (suc2 == GREATER_EQUAL)
+                {                               // pro E -> E>=E
+                    printf("E -> E>=E\n");
+                    return 6;							// return 6
+                }
+                if (suc2 == LESS)
+                {                               // pro E -> E<E
+                    printf("E -> E<E\n");
+                    return 7;							// return 7
+                }
+                if (suc2 == LESS_EQUAL)
+                {                               // pro E -> E<=E
+                    printf("E -> E<=E\n");
+                    return 8;							// return 8
+                }
+                if (suc2 == EQUAL)
+                {                               // pro E -> E==E
+                    printf("E -> E==E\n");
+                    return 9;							// return 9
+                }
+                if (suc2 == NEGATION)
+                {                               // pro E -> E!=E
+                    printf("E -> E!=E\n");
+                    return 10;							// return 10
+                }
+            }
 		break;
+            
 	case 'i':									// pro E -> i
 		printf("E -> i\n");
-		return 11;									// return 11
+		return 11;                                      // return 11
+            
 	case L_BRACKET:								// pro E -> (E)
 		if (suc2 == E) {
 			if (suc3 == R_BRACKET) {
@@ -131,39 +169,56 @@ switch (suc1) {
 //------------------------------//
 //	PRECEDENCNI SYNTAX ANALYZA	//
 //------------------------------//
-int PrecedencniSA () {
+
+int PrecedencniSA ()
+{
 	int val, top, input, suc1, suc2, suc3;				// promenne
 	tList *Z;											// zasobnik
-	Init(Z);
+	
+    
+    Init(Z);
 	Push(Z, SEMICOLON);
 	NextToken(&input);									// input
 	FirstTerminal(Z, &top);								// nacteni pocatku zasobniku
-	while (top != SEMICOLON && input != SEMICOLON) {	// dokud není vstup i zasobnik prazdny
+	
+    while (top != SEMICOLON && input != SEMICOLON)
+    {                                                   // dokud není vstup i zasobnik prazdny
 		FirstTerminal(Z, &top);							// nacti dalsi terminal
-		switch (precedencni_tabulka[OperatorToIndex(top)][OperatorToIndex(input)])	// najdi pravidlo v prec. tabulce
-			case '=': 									// pokud se rovnaji
+		
+        switch (precedencni_tabulka[OperatorToIndex(top)][OperatorToIndex(input)])	// najdi pravidlo v prec. tabulce
+			
+            case '=': 									// pokud se rovnaji
 				push(Z, input);							// zapis do zasobniku
 				input = NextToken();					// nacti dalsi znak na vstupu
 				break;
+        
 			case '<': 									// pokud ma zasobnik mensi prioritu nez vstup (dle tabulky)
 				Push(Z, '<');							// zapis do zasobniku znak <
 				Push(Z, input);							// zapis vstup
 				input = NextToken();					// nacti dalsi vstup
 				break;
+        
 			case '>':									// pokud ma zasobnik vetsi prioritu nez vstup (dle tabulky)
-				do {
+				do
+                {
 					Pop(Z, top);						// vytahni prvek na top
 					suc3 = suc2;						// zapis 3. predchudce
 					suc2 = suc1;						// zapis 2. predchudce
 					suc1 = top;							// zapis prvniho predchudce
 				} while (top != '<')					// dokud v promennych nemam cely retezec, pro ktery hledam pravidlo
+                    
 				if (top == '<' && PrecG(suc1, suc2, suc3) != 0) Push(Z, 'E');	// kontrola na nalezene pravidlo, pokud existuje, prepise na E -> neterminal
 				else error();
-				break;
+                break;
+        
 			case ' ': 									// chyba ve vyrazu
 				error();
-				break;
+                break;
 	}
-	Dispose(Z);											// uvolneni zasobniku
+    
+	Dispose(Z);                                         // uvolneni zasobniku
+    
 	printf("JOOO\n");									// radostne povzdechnuti
+    
+    return ERR_None;
 }
