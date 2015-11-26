d37750a079ba371ae1a2787f4ede829676f26fde
Valentine <valentina.strakova@gmail.com>
2015-11-26 01:34:30 +0100

    added SIMPLE and WHILE extensions

--- parser.h
+++ parser.h
@@ -4,13 +4,14 @@
                                                     /*Function definitions*/
 
 int parse(FILE *source);                    // parse the code
+int isKeyword(int tokenType);
 
 
 //functions simulating LL grammar rules             TODO
 
 int rule_stList();              // <st-list> -> <statement> <st-list>     |   } 
 
-int rule_statement();           // <statement> -> type id <var-def> ;   ||      id = <expression> ;      ||        id (<param-list>) ;      ||      <keyword>
+int rule_statement();           // <statement> -> type id <var-def> ;   ||      id = <expression> ;      ||        id (<param-list>) ;      ||      <keyword>	|| { <st-list>
 int rule_varDecl();             // <var-decl> -> ;    |   = <expression> ;
 
 int rule_funcdef();             // <prog> -> type id <param-list> <func-defined> 
@@ -20,7 +21,7 @@ int rule_param();               // <param> -> type id
 int rule_paramNext();           // <param-next> -> , <param> <param-next>       |   )
 
 int rule_keyword();             // <keyword> -> auto <auto-decl>    ||      cin <cin> <cin-list>    ||  cout <cout> <cout-list>
-                                // ||   for <for-decl>  ||  if <if-decl>    ||  return <rule-return>
+                                // ||   for <for-decl>  ||  if <if-decl>    ||  return <rule-return>	|| while <while_loop>	|| do <do-loop>
 
 int rule_auto();                // <auto-decl> -> id = <expression>
 int rule_cin();                 // <cin> -> >> id
@@ -29,7 +30,9 @@ int rule_cout();                // <cout> -> << id
 int rule_coutList();            // <cout-list> -> <cout> <cout-list>   ||  ;
 int rule_for();                 // <for-decl> -> ( id <var-decl> <expression> id = <expression> ) <st-list>
 int rule_if();                  // <if-decl> -> ( <expression> ) <st-list> else <st-list>
-int rule_return();				// <return> -> expression               
+int rule_return();				// <return> -> expression
+int rule_while();				// <while-loop> -> ( <expression> ) <statement>
+int rule_do();					// <do-loop> -> <statement> while ( <expression> )               
 
 
 int rule_expression();
