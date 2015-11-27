3ec175e4d5dd50b0272855c583b56a4394640f71
DWCZ <darkwizard@email.cz>
2015-11-27 14:35:44 +0100

    added struct instrlist

--- prsa.h
+++ prsa.h
@@ -4,18 +4,42 @@
 #ifndef PRSA_H
 #define PRSA_H
 
+#define NETERMINAL 500
+#define TERMINAL 501
+
+typedef struct {
+    int terminal;
+    int type;
+    void *data;
+} tExpr;
 // ZASOBNIK
-typedef struct tElem {		// deklarace struktury
+typedef struct tElem {      // deklarace struktury
     struct tElem *ptr;
-    int data;
-} *tElemPtr;	               
-typedef struct { 			// ukazatel prvniho prvku
+    tExpr* data;
+} *tElemPtr;                   
+typedef struct {            // ukazatel prvniho prvku
     tElemPtr First;
 } tList;
+// LINEARNI SEZNAM
+typedef struct {
+    int operator;
+    int type;
+    void *input1;
+    void *input2;
+    void *output;
+} tInstruction;
+typedef struct instructListElem {
+    struct instructListElem *next;
+    tInstruction *instruction;
+} tInstrListElem;                  
+typedef struct { 
+    tInstrListElem *first;
+    tInstrListElem *last;
+} tInstrList;
 
 // PRECEDENCNI TABULKA
-char precedencni_tabulka[][] = {
-//		   *   /   +   -   >   >=  <   <=  ==  !=  (   )   i   ;
+char precedencni_tabulka[14][14] = {
+//         *   /   +   -   >   >=  <   <=  ==  !=  (   )   i   ;
 /* *  */ {'>','>','>','>','>','>','>','>','>','>','<','>','<','>'}, 
 /* /  */ {'>','>','>','>','>','>','>','>','>','>','<','>','<','>'},
 /* +  */ {'<','<','>','>','>','>','>','>','>','>','<','>','<','>'},
@@ -33,23 +57,27 @@ char precedencni_tabulka[][] = {
 }
 
 // OPERACE TABULKY
-char OperatorToIndex (char* op) {
-	switch (op[0]) {
-		case MULTIPLY: 		return 0;	// *	= 0
-		case DIVIDE: 		return 1; 	// /	= 1
-		case PLUS: 			return 2; 	// + 	= 2 
-		case MINUS: 		return 3; 	// - 	= 3
-		case GREATER: 		return 4;	// > 	= 4
-		case GREATER_ROVNO:	return 5;	// >=	= 5
-		case LESS: 			return 6;	// < 	= 6
-		case LESS_ROVNO: 	return 7;	// <= 	= 7 
-		case EQUAL: 		return 8; 	// == 	= 8
-		case NEGACE: 		return 9; 	// != 	= 9
-		case L_BRACKET: 	return 10; 	// ( 	= 10
-		case R_BRACKET: 	return 11; 	// ) 	= 11
-		case IDENTIFIER: 	return 12; 	// i 	= 12 <----- dodelat
-		case SEMICOLON: 	return 13; 	// ; 	= 13
-	}
+char OperatorToIndex (tExpr* op) {
+    switch (op->type) {
+        case MULTIPLY:      return 0;   // *    = 0
+        case DIVIDE:        return 1;   // /    = 1
+        case PLUS:          return 2;   // +    = 2 
+        case MINUS:         return 3;   // -    = 3
+        case GREATER:       return 4;   // >    = 4
+        case GREATER_ROVNO: return 5;   // >=   = 5
+        case LESS:          return 6;   // <    = 6
+        case LESS_ROVNO:    return 7;   // <=   = 7 
+        case EQUAL:         return 8;   // ==   = 8
+        case NEGACE:        return 9;   // !=   = 9
+        case L_BRACKET:     return 10;  // (    = 10
+        case R_BRACKET:     return 11;  // )    = 11
+        case DOUBLE_NUMBER:
+        case INT_NUMBER:
+        case STRING:
+        case IDENTIFIER:    return 12;  // i    = 12 <----- dodelat
+        case SEMICOLON:     return 13;  // ;    = 13
+        default error;
+    }
 }
 
 int PrecedencniSA ();
