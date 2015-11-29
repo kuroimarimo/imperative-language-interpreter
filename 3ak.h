8cd91126f18357ac529d6f8c7f487e41b56f92a6
filvas <filip.vasko@yahoo.com>
2015-11-29 19:00:53 +0100

    added  #ifndef macro

--- 3ak.h
+++ 3ak.h
@@ -1,24 +1,29 @@
+#ifndef AK_H
+#define AK_H
+
 enum adressCode {
-OP_ASSIGN = 0,	// prirazeni in1 -> out
-OP_SUM,			// secteni in1, in2 -> out
-OP_MINUS,		// odecteni in1, in2 -> out
-OP_MUL,			// nasobeni in1, in2 -> out
-OP_DIV,			// deleni in1, in2 -> out
-OP_MOD,			// modulo in1, in2 -> out
-OP_LT,			// in1 mensi nez in2 -> out
-OP_GT,			// in1 vetsi nez in2 -> out
-OP_LE,			// in1 mensirovno in2 -> out
-OP_GE,			// in1 vetsirovno in2 -> out
-OP_EQUALS,		// in1 rovna se in2 -> out
-OP_DIFF,		// in1 ruzne od in2 -> out
-OP_INT,			// aloc. 4 bajtu -> out
-OP_DOUBLE,		// aloc 8 bajtu -> out
-OP_STRING,		// aloc x bajtu -> out
-OP_COUT,		// >> dalsi COUT, vystup -> out
-OP_CIN,			// << dalsi CIN, vstup -> out
-OP_IF,			// pokud plati in1 -> out
-OP_ELSE, 		// pokud neplati in1 -> out
-OP_LABEL,		// in1 <- navesti pro goto
-OP_GOTO,		// skok pro for -> out
-OP_RETURN		// in1 ulozi -> out
+	OP_ASSIGN = 0,	// prirazeni in1 -> out
+	OP_SUM,			// secteni in1, in2 -> out
+	OP_MINUS,		// odecteni in1, in2 -> out
+	OP_MUL,			// nasobeni in1, in2 -> out
+	OP_DIV,			// deleni in1, in2 -> out
+	OP_MOD,			// modulo in1, in2 -> out
+	OP_LT,			// in1 mensi nez in2 -> out
+	OP_GT,			// in1 vetsi nez in2 -> out
+	OP_LE,			// in1 mensirovno in2 -> out
+	OP_GE,			// in1 vetsirovno in2 -> out
+	OP_EQUALS,		// in1 rovna se in2 -> out
+	OP_DIFF,		// in1 ruzne od in2 -> out
+	OP_INT,			// aloc. 4 bajtu -> out
+	OP_DOUBLE,		// aloc 8 bajtu -> out
+	OP_STRING,		// aloc x bajtu -> out
+	OP_COUT,		// >> dalsi COUT, vystup -> out
+	OP_CIN,			// << dalsi CIN, vstup -> out
+	OP_IF,			// pokud plati in1 -> out
+	OP_ELSE, 		// pokud neplati in1 -> out
+	OP_LABEL,		// in1 <- navesti pro goto
+	OP_GOTO,		// skok pro for -> out
+	OP_RETURN		// in1 ulozi -> out
 }; 
+
+#endif
