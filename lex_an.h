eb5d5a0f674a34591018ecad15da9896ec5a8f57
Vojta <parmen.v@seznam.cz>
2015-12-01 21:51:53 +0100

    Actualize

---
+++ lex_an.h
@@ -0,0 +1,143 @@
+#ifndef LEX_AN_H
+#define LEX_AN_H
+
+
+
+/** jednotlivé stavy */
+enum {
+    START,              /// 0       Určení znaku
+
+    NUMBER,             /// 1       0..9
+    LETTER,             /// 2       a..z, A..Z
+    NUMBER_START_ZERO,
+    DEC_NUMBER,         /// 4       1.234
+    DEC_NUMBER_END,     /// 5       *.123
+    EXP_NUMBER,         /// 6       1e+4 15E8
+    EXP_NUMBER_SIGN,    /// 7       znamenkovy (+/-) 15E+8
+    EXP_NUMBER_END,     /// 8
+
+    SEMICOLON = 10,     ///         ;
+    UNDERSCORE,         ///         _
+
+    L_BRACKET,          ///         (
+    R_BRACKET,          ///         )
+    L_SQUARE_BRACKET,   ///         [
+    R_SQUARE_BRACKET,   ///         ]
+    L_CURLY_BRACKET,    ///         {
+    R_CURLY_BRACKET,    ///         }
+
+    PLUS,               ///         +
+    MINUS,              ///         -
+    INCREMENT,          ///         ++
+    DECREMENT,          ///         --
+    MULTIPLY,           ///         *
+    DIVIDE,             ///         /
+    BACKSLASH,          ///
+    MODULO,             ///         %
+
+    EXCLAMATION_MARK,   ///         !
+    NEGATION,           ///         !=
+    QUESTION_MARK,      ///         ?
+    STRING,             ///         ".."
+    STRING_ESCAPE,      ///
+    STRING_ESCAPE_x1,   ///
+    STRING_ESCAPE_x2,   ///
+    COMMA,              ///         ,
+
+    LESS,               ///         <
+    GREATER,            ///         >
+    C_IN,               ///         >>
+    C_OUT,              ///         <<
+    ASSIGNMENT,         ///         =
+    EQUAL,              ///         ==
+    LESS_EQUAL,         ///         <=
+    GREATER_EQUAL,      ///         >=
+
+    LINE_COMMENT,       ///         // ...
+    COMMENT,            ///         /* ...
+    COMMENT_END,        ///         ... */
+
+    ANOTHER_CHAR,       ///         ostatni znaky
+
+
+    TOK_NULL,           ///         prazdny/neinicializovany token  */
+
+    BINARY,             ///         dvojkove cislo
+    OCTAL,              ///         osmickove cislo
+    HEXADECIMAL,        ///         hexadecimalni cislo
+
+        /// typy tokenu
+    IDENTIFIER = 60,    ///
+    KEY_WORD,           ///
+    BUILT_IN_FUNCTION,  ///
+    INT_NUMBER,         ///
+    DOUBLE_NUMBER,      ///
+
+        /// klicova slova
+    K_CIN = 70,         ///
+    K_COUT,             ///
+    K_AUTO,             ///
+    K_INT,              ///
+    K_DOUBLE,           ///
+    K_STRING,           ///
+    K_IF,               ///
+    K_ELSE,             ///
+    K_FOR,              ///
+    K_RETURN,           ///
+    K_WHILE,            ///
+    K_DO,               ///
+
+        /// vestavene funkce
+    B_LENGTH = 85,      ///
+    B_SUBSTR,           ///
+    B_CONCAT,           ///
+    B_FIND,             ///
+    B_SORT,             ///
+
+    MAX_OF_ENUM         ///
+
+};
+
+/*
+union unie {
+    int int_number;
+    double double_number;
+    char *area;
+} unie;
+*/
+
+
+typedef struct {
+    char *area;
+    int int_numb;
+    double double_numb;
+
+    int type;
+    int counter_of_lines;
+
+    int counter;
+    int sizeof_area;
+} tToken;
+
+
+#define COUNT_OF_KEY_WORDS 12
+#define COUNT_OF_BUILT_IN_FUNCTIONS 5
+
+    /// globalni promenne
+tToken token, oldToken;
+FILE *source;
+
+    /// prototypy funkci
+
+void ungetToken();               //ungets the latest token, can be used only once in a row
+void tokenSwap();
+int tokenCopy (tToken *dst, tToken src);   //"copies" the contents of src token to the dst token
+void initToken (); // function for inicialization Token .. it's called when you need delete old variables
+void fillToken (char character); // comes some character and function saves it to variable
+int scanner ();  // its scanner, heart of lex_an ... it's automata
+
+
+
+int ungotToken;                 //TODO: inicializovať na 0 >_<
+
+#endif
