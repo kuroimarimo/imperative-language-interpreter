a009b7400cd2412ad01160e7fcbb60a797b2efc5
Valentine <valentina.strakova@gmail.com>
2015-12-13 22:26:49 +0100

    fixed misspelling

--- instructions.h
+++ instructions.h
@@ -15,8 +15,8 @@
  *  https://github.com/kuroimarimo/imperative-language-interpreter
  *
  *  Team:
- *  Votjěch Václavík	(xvacla22)
- *  Peter Vančo			(xvanco05)
+ *  Vojtěch Václavík	(xvacla22)
+ *  Petr Vančo			(xvanco05)
  *  Filip Vaško         (xvasko10)
  *  Dominik Vašek		(xvasek06)
  *  Valentína Straková	(xstrak27)
